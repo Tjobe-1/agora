fb6e4c1c7ae429178d10003c1691a5018a2ddd0b
Tjobe-1 <87443885+Tjobe-1@users.noreply.github.com>
2022-05-24 09:24:50 +0200

    Baserow

---
+++ src/routes/api/index.js
@@ -0,0 +1,91 @@
+import axios from "axios";
+
+// axios.<method> will now provide autocomplete and parameter typings
+
+const API_KEY = "2uSBXlWAJGjDabr7pWGX2gk63GOjznLQ";
+
+const headers = { headers: { authorization: "Token " + API_KEY } };
+
+async function getFields(table) {
+  try {
+    const result = await axios.get(
+      "https://agora-baserow.herokuapp.com/api/database/fields/table/" + table,
+      headers
+    );
+
+    if (result.status == 200) {
+      return result;
+    } else {
+      console.log(error);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  result = await axios.get(
+    "https://agora-baserow.herokuapp.com/api/database/fields/table/" + table,
+    headers
+  );
+
+  return result;
+}
+
+async function getRows(table) {
+  try {
+    const result = await axios.get(
+      "https://agora-baserow.herokuapp.com/api/database/rows/table/" +
+        table +
+        "/?user_field_names=true",
+      headers
+    );
+
+    if (result.status == 200) {
+      return result;
+    } else {
+      console.log(error);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function processRaw() {
+  res = await getRows(3);
+  returnData = [];
+
+  res.data.results.forEach((element) => {
+    apObj = {};
+
+    apObj.id = element.id;
+
+    apObj.image = "";
+    apObj.meta = [];
+    apObj.links = [];
+    apObj.content = element.Content;
+    apObj.title = element.Name;
+
+    apObj.meta.push("Participant: " + element.Participant);
+
+    if (element.Image.length > 0) {
+      apObj.image = element.Image[0].url;
+    }
+
+
+    if (element.Analysis.length > 0) {
+      element.Analysis.forEach((link) => {
+        apObj.links.push(link.value);
+      });
+    }
+
+    returnData.push(apObj);
+  });
+
+  return returnData;
+}
+
+export async function get() {
+  res = await processRaw();
+
+  return {
+    body: res,
+  };
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,66 +1,114 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
+
+  export async function load({ fetch }) {
+    const res = await fetch("/api");
+
+    if (res.ok) return { props: { cards: await res.json() } };
+    return {
+      status: res.status,
+      error: new Error(),
+    };
+  }
 </script>
 
 <script lang="ts">
-	import Masonry from 'svelte-bricks';
+  import Masonry from "svelte-bricks";
 
-	let [minColWidth, maxColWidth, gap] = [300, 800, 20];
-	let width, height;
+  let [minColWidth, maxColWidth, gap] = [300, 800, 20];
+  let width, height;
 
-	export let result: string;
-	let items = [];
-	$: items = JSON.parse(result);
+  // export let result: string;
+  // let items = [];
+  // $: items = JSON.parse(result);
+
+  export let cards;
+
+  let items = [];
+  $: items = cards;
 </script>
 
 <div bind:clientWidth={width} bind:clientHeight={height} />
 
 <h1>Welcome to Agora!</h1>
 
-<div class="btn-toolbar my-4" role="toolbar" aria-label="Toolbar with button groups">
-	<div class="btn-group me-4 mb-2" role="group" aria-label="First group">
-		<input
-			type="radio"
-			class="btn-check"
-			name="btnradio"
-			id="btnradio1"
-			autocomplete="off"
-			checked
-		/>
-		<label class="btn btn-outline-secondary" for="btnradio1">Take</label>
+<div
+  class="btn-toolbar my-4"
+  role="toolbar"
+  aria-label="Toolbar with button groups"
+>
+  <div class="btn-group me-4 mb-2" role="group" aria-label="First group">
+    <input
+      type="radio"
+      class="btn-check"
+      name="btnradio"
+      id="btnradio1"
+      autocomplete="off"
+      checked
+    />
+    <label class="btn btn-outline-secondary" for="btnradio1">Take</label>
 
-		<input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" />
-		<label class="btn btn-outline-secondary" for="btnradio2">Another</label>
+    <input
+      type="radio"
+      class="btn-check"
+      name="btnradio"
+      id="btnradio2"
+      autocomplete="off"
+    />
+    <label class="btn btn-outline-secondary" for="btnradio2">Another</label>
 
-		<input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off" />
-		<label class="btn btn-outline-secondary" for="btnradio3">Perspective</label>
-	</div>
-	<div class="input-group me-4 mb-2">
-		<div class="input-group-text" id="btnGroupAddon">@</div>
-		<input
-			type="text"
-			class="form-control"
-			placeholder="Participant #"
-			aria-label="Input group example"
-			aria-describedby="btnGroupAddon"
-		/>
-	</div>
+    <input
+      type="radio"
+      class="btn-check"
+      name="btnradio"
+      id="btnradio3"
+      autocomplete="off"
+    />
+    <label class="btn btn-outline-secondary" for="btnradio3">Perspective</label>
+  </div>
+  <div class="input-group me-4 mb-2">
+    <div class="input-group-text" id="btnGroupAddon">@</div>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Participant #"
+      aria-label="Input group example"
+      aria-describedby="btnGroupAddon"
+    />
+  </div>
 </div>
 
-<Masonry {items} {minColWidth} {maxColWidth} {gap} let:item bind:width bind:height>
-	<div class="card">
-		{#if item.content != undefined}
-			{#each item.content as content}
-				<img src={content.url} class="card-img-top" />
-			{/each}
-		{/if}
-		<div class="card-body">
-			<h5 class="card-title">{item.title}</h5>
-			{#each item.links as link}
-				{#if link != null}
-					<a href="/#" class="card-link">{link}</a>
-				{/if}
-			{/each}
-		</div>
-	</div>
+<Masonry
+  {items}
+  {minColWidth}
+  {maxColWidth}
+  {gap}
+  let:item
+  bind:width
+  bind:height
+>
+  <div class="card">
+    {#if item.image != ""}
+      <img src={item.image} alt="images" class="card-img-top" />
+    {/if}
+
+    <div class="card-body">
+      <h5 class="card-title">{item.title}</h5>
+      <a href="/#" class="card-link">
+        {#each item.meta as meta}
+          {meta}
+        {/each}
+      </a>
+
+          {#if item.content != null}
+            <p class="card-text">{item.content}</p>
+          {/if}
+          {#each item.links as link}
+            {#if link != null}
+              <a href="/#" class="card-link">{link}</a>
+            {/if}
+          {/each}
+
+    </div>
+  </div>
 </Masonry>
