9eb134a17bf9eb0aed1ccde8f5f24c6c07e8811c
Tjobe-1 <87443885+Tjobe-1@users.noreply.github.com>
2022-06-05 21:04:08 +0200

    added pages

---
+++ src/routes/opt-in.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Typeform from 'svelte-typeform-embed';
+    function submitted () {
+        window.location.href = '/';
+    }
+</script>
+
+<div class="container">
+    <Typeform url="https://ttvbussel.typeform.com/to/OGYV2t9Q" onSubmit={submitted}/>
+</div>
